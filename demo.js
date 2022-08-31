ff645ac4fbc3bf965019e208f27e8ccd74f7574d
Sudha Krishnamoorthy <sudhavkr@hotmail.com>
2022-08-30 21:47:26 -0500

    Initial commit demo

---
+++ demo.js
@@ -0,0 +1,39 @@
+let jonSnowHealth = 100
+let strHealth = String(jonSnowHealth)
+jonSnowHealth = jonSnowHealth +''
+
+jonSnowHealth = Boolean(jonSnowHealth)
+jonSnowHealth = Number(jonSnowHealth)
+console.log(typeof jonSnowHealth)
+
+
+let myWinner = 'John is the winner';
+myWinner = myWinner.replace('John', 'Danny')
+if (myWinner.includes('John')) {
+    console.log(myWinner)
+}
+//camelCase (used in JS)
+//PascalCase
+//kebab-case
+//snake_case
+
+let mySplitStr = myWinner.split("")
+console.log(mySplitStr)
+let myAlmostKebabStr = mySplitStr.join('-')
+console.log(myAlmostKebabStr)
+let myKebabStr = myAlmostKebabStr.toLowerCase()
+
+//const myKebabCaseStr = myWinner.split("").join("-").toLowerCase()
+const myKebabCaseStr = myWinner.toLowerCase().replaceAll ('', '-')
+console.log({myKebabCaseStr})
+
+
+const isJonAlive = () => {
+    if(jonSnowHealth > 100) {
+        console.log('Jon is alive')
+    }
+
+else {
+    console.log('Jon is dead')
+}}
+isJonAlive() 
